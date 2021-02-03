c7279a5a8b2133689fc9cc092e3c3fa4d9a29303
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-02-04 00:04:23 +0200

    get currLocation

--- js/map-controller.js
+++ js/map-controller.js
@@ -3,23 +3,72 @@
 function onInitMap() {
   let elBody = document.querySelector('body');
   onChangePageClrs(elBody);
-  initMap(lat, lng);
+  initMap();
+}
+
+function mapReady() {
+    console.log('The map is ready');
 }
 
 function initMap(lat, lng) {
-  if (!lat) lat = 32.0749831;
-  if (!lng) lat = 34.9120554;
-  var elMap = document.querySelector('#map');
-  var options = {
+  if (!lat) lat = 29.55805;
+  if (!lng) lng = 34.94821;
+  let elMap = document.querySelector('#map');
+  let options = {
     center: { lat, lng },
-    zoom: 16,
-  };
-
+    zoom: 15,
+  }
   var map = new google.maps.Map(elMap, options);
 
   var marker = new google.maps.Marker({
-    position: { lat, lng },
-    map,
-    title: 'Hello World!',
-  });
+        position: { lat, lng },
+        map,
+        title: 'Hello World!',
+      });
+}
+
+function getCurrLocation() {
+    getPosition();
+}
+
+function getPosition() {
+    if (!navigator.geolocation) {
+        alert("HTML5 Geolocation is not supported in your browser.");
+        return;
+    }
+
+    // One shot position getting or continus watch
+    navigator.geolocation.getCurrentPosition(showLocation, handleLocationError);
+    // navigator.geolocation.watchPosition(showLocation, handleLocationError);
+}
+
+function showLocation(position) {
+    console.log(position);
+    document.getElementById("latitude").innerHTML = position.coords.latitude;
+    document.getElementById("longitude").innerHTML = position.coords.longitude;
+    document.getElementById("accuracy").innerHTML = position.coords.accuracy;
+
+    var date = new Date(position.timestamp);
+    document.getElementById("timestamp").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    initMap(position.coords.latitude, position.coords.longitude);
+    // map.setCenter(new google.maps.LatLng( position.coords.latitude, position.coords.longitude ));
+}
+
+function handleLocationError(error) {
+    var locationError = document.getElementById("locationError");
+
+    switch (error.code) {
+        case 0:
+            locationError.innerHTML = "There was an error while retrieving your location: " + error.message;
+            break;
+        case 1:
+            locationError.innerHTML = "The user didn't allow this page to retrieve a location.";
+            break;
+        case 2:
+            locationError.innerHTML = "The browser was unable to determine your location: " + error.message;
+            break;
+        case 3:
+            locationError.innerHTML = "The browser timed out before retrieving the location.";
+            break;
+    }
 }
