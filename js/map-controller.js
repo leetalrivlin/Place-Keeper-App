3b10e151a0b14edc875edbd95f92719412b8bc03
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-02-03 22:01:25 +0200

    starting the map

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,6 +1,25 @@
 'use strict';
 
 function onInitMap() {
-    let elBody = document.querySelector('body');
-    onChangePageClrs(elBody);
+  let elBody = document.querySelector('body');
+  onChangePageClrs(elBody);
+  initMap(lat, lng);
+}
+
+function initMap(lat, lng) {
+  if (!lat) lat = 32.0749831;
+  if (!lng) lat = 34.9120554;
+  var elMap = document.querySelector('#map');
+  var options = {
+    center: { lat, lng },
+    zoom: 16,
+  };
+
+  var map = new google.maps.Map(elMap, options);
+
+  var marker = new google.maps.Marker({
+    position: { lat, lng },
+    map,
+    title: 'Hello World!',
+  });
 }
